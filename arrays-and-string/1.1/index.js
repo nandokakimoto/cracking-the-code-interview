8d45a3ce2e2378e873c6fdef8cd95e97eb4847c9
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-04-18 22:39:26 -0300

    Implements iterative version of isUnique.

--- arrays-and-string/1.1/index.js
+++ arrays-and-string/1.1/index.js
@@ -1,4 +1,19 @@
-function isUnique(input, start, end, bitVector) {
+function isUniqueI(input) {
+  var bitVector = {};
+
+  for (var i = 0; i < input.length; i++) {
+    var char = input[i];
+
+    if (bitVector[char]) {
+      return false;
+    } else {
+      bitVector[char] = true;
+    }
+  }
+  return true;
+}
+
+function isUniqueR(input, start, end, bitVector) {
   if (start === end) { // there is a single character
     var char = input[start];
 
@@ -13,11 +28,14 @@ function isUnique(input, start, end, bitVector) {
     var middle = Math.floor((end - start) / 2);
 
     // recurse left and right of the array
-    return isUnique(input, start, start + middle, bitVector) &&
-      isUnique(input, start + middle + 1, end, bitVector);
+    return isUniqueR(input, start, start + middle, bitVector) &&
+      isUniqueR(input, start + middle + 1, end, bitVector);
   }
 }
 
-module.exports = function(input) {
-  return isUnique(input, 0, input.length - 1, {});
+module.exports = {
+  iterative: isUniqueI,
+  recursive: function(input) {
+    return isUniqueR(input, 0, input.length - 1, {});
+  }
 }
--- arrays-and-string/1.1/test/test.js
+++ arrays-and-string/1.1/test/test.js
@@ -2,16 +2,33 @@ const isUnique = require('../index.js');
 const assert = require('assert');
 
 describe('isUnique', function() {
-  it('should return true when all characteres are unique', function(){
-    assert(isUnique('abcdef'));
-    assert(isUnique('qwerty'));
-    assert(isUnique('qazplmwsxoknedcijb'));
-    assert(isUnique('123456abcdef7890'));
+  describe('iterative: O(N)', function (){
+    it('should return true when all characteres are unique', function(){
+      assert(isUnique.iterative('abcdef'));
+      assert(isUnique.iterative('qwerty'));
+      assert(isUnique.iterative('qazplmwsxoknedcijb'));
+      assert(isUnique.iterative('123456abcdef7890'));
+    });
+
+    it('should return false when duplicated characteres exists', function() {
+      assert(!isUnique.iterative('abccdef'));
+      assert(!isUnique.iterative('qazxswedcvfrvtgbyhnujmiklop'));
+      assert(!isUnique.iterative('123456789abcdefghij1klmnopq'));
+    });
   });
 
-  it('should return false when duplicated characteres exists', function() {
-    assert(!isUnique('abccdef'));
-    assert(!isUnique('qazxswedcvfrvtgbyhnujmiklop'));
-    assert(!isUnique('123456789abcdefghij1klmnopq'));
+  describe('recursive: O(N*log(N))', function (){
+    it('should return true when all characteres are unique', function(){
+      assert(isUnique.recursive('abcdef'));
+      assert(isUnique.recursive('qwerty'));
+      assert(isUnique.recursive('qazplmwsxoknedcijb'));
+      assert(isUnique.recursive('123456abcdef7890'));
+    });
+
+    it('should return false when duplicated characteres exists', function() {
+      assert(!isUnique.recursive('abccdef'));
+      assert(!isUnique.recursive('qazxswedcvfrvtgbyhnujmiklop'));
+      assert(!isUnique.recursive('123456789abcdefghij1klmnopq'));
+    });
   });
 });
