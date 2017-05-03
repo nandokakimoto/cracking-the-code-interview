68d7f01b00fecd72d04f28672bc11b1e630464aa
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-05-02 23:50:38 -0300

    Implements solution to 1.5

--- arrays-and-string/1.5/solution.js
+++ arrays-and-string/1.5/solution.js
@@ -1,3 +1,38 @@
-module.exports = function(input1, input2) {
-  return false;
+function compare(bigger, smaller) {
+  var i = 0;
+  var j = 0;
+  var diff = 0
+
+  while(i < bigger.length && j < smaller.length && diff < 2) {
+    if (bigger[i] === smaller[j]) {
+      i += 1;
+      j += 1;
+    } else {
+      if (bigger.length === smaller.length) {
+        j += 1;
+      }
+      diff += 1;
+      i += 1;
+    }
+  }
+
+  return diff;
+}
+
+module.exports = function oneAway(input1, input2) {
+  var lengthDiff = input1.length - input2.length;
+  var diff = 0;
+
+  // return false if more tha one character diff
+  if (lengthDiff < -1 || lengthDiff > 1) {
+    return false;
+  }
+
+  if (lengthDiff < 0) { // input2 is bigger
+    diff = compare(input2, input1);
+  } else { // input1 is bigger os similar
+    diff = compare(input1, input2);
+  }
+
+  return diff < 2;
 };
--- arrays-and-string/1.5/test/solution_test.js
+++ arrays-and-string/1.5/test/solution_test.js
@@ -9,25 +9,28 @@ describe('one away', function(){
 
   it('should return true when strings are one edit away', function() {
     // beginning of string
-    assert(oneAway('character', 'haracter'));
-    assert(oneAway('character', 'xcharacter'));
-    assert(oneAway('character', 'zharacter'));
+    assert(oneAway('character', 'haracter'));   // remove
+    assert(oneAway('character', 'xcharacter')); // add
+    assert(oneAway('character', 'zharacter'));  // replace
 
     // end of string
-    assert(oneAway('character', 'characte'));
-    assert(oneAway('character', 'characters'));
-    assert(oneAway('character', 'charactex'));
+    assert(oneAway('character', 'characte'));   // remove
+    assert(oneAway('character', 'characters')); // add
+    assert(oneAway('character', 'charactex'));  // replace
 
     // middle of string
-    assert(oneAway('character', 'charcter'));
-    assert(oneAway('character', 'charaacter'));
-    assert(oneAway('character', 'charecter'));
+    assert(oneAway('character', 'charcter'));   //remove
+    assert(oneAway('character', 'charaacter')); // add
+    assert(oneAway('character', 'charecter'));  // replace
   });
 
   it('should return false when strings are more than one edit away', function() {
     assert(!oneAway('cccharacter', 'character'));
     assert(!oneAway('character', 'characterrr'));
     assert(!oneAway('ccharacter', 'characterr'));
-    assert(!oneAway('charaacter', 'characcter'));
+    assert(!oneAway('character', 'charcater'));
+    assert(!oneAway('This is a sentence', 'This is also a sentence'));
+    assert(!oneAway('aaaaaaa', 'ccccccc'));
+    assert(!oneAway('aaaaaaa', 'baaaaaaac'));
   });
 });
