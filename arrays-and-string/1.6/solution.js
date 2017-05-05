18a16d28fe6c00b13a1778bf447b07df1ef4b6af
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-05-04 22:32:26 -0300

    Implements solution to 1.6 question.

--- arrays-and-string/1.6/solution.js
+++ arrays-and-string/1.6/solution.js
@@ -1,3 +1,23 @@
+function append(input, char,  count) {
+  return input + char + count.toString();
+}
+
 module.exports = function(input) {
-  return null;
+  var char = input[0];
+  var count = 1;
+  var result = '';
+
+  for(var i = 1; i < input.length; i++) {
+    var current = input[i];
+    if(char === current) {
+      count += 1;
+    } else {
+      result = append(result, char, count);
+      char = current;
+      count = 1;
+    }
+  }
+
+  result = append(result, char, count);
+  return result.length < input.length ? result : input;
 };
--- arrays-and-string/1.6/test/solution_test.js
+++ arrays-and-string/1.6/test/solution_test.js
@@ -2,11 +2,18 @@ var assert = require('assert');
 var compress = require('../solution.js');
 
 describe('String compression', function() {
-  it('should return compress strings when they are smaller', function() {
-    assert.equal('a2b1c5a3', compress('aabbcccaa'));
+  it('returns compressed string when it is are smaller than original', function() {
+    assert.equal('a2b1c5a3', compress('aabcccccaaa'));
+    assert.equal('a8', compress('aaaaaaaa'));
+    assert.equal('a8b1c1d1', compress('aaaaaaaabcd'));
+    assert.equal('a8A6', compress('aaaaaaaaAAAAAA'));
+    assert.equal('a8b1a3', compress('aaaaaaaabaaa'));
+    assert.equal('a1b8a1b1a1b1', compress('abbbbbbbbabab'));
   });
 
-  it('should return original string when it is smaller', function() {
+  it('returns original string when compression does not return smaller string', function() {
     assert.equal('abcd', compress('abcd'));
+    assert.equal('aabbccdd', compress('aabbccdd'));
+    assert.equal('abbcdd', compress('abbcdd'));
   });
 });
