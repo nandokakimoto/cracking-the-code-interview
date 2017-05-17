0bbd95b909ec7050f71df93251f531cc461e82fd
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-05-16 23:55:26 -0300

    Refactor solution 1.8

--- arrays-and-string/1.8/solution1.js
+++ arrays-and-string/1.8/solution1.js
@@ -5,27 +5,25 @@ module.exports = function(matrix) {
   for(var i = 0; i < matrix.length; i++) {
     for(var j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === 0) {
-        if(rows.indexOf(i) === -1){
-          rows.push(i);
-        }
-        if(columns.indexOf(j) === -1){
-          columns.push(j);
-        }
+        rows[i] = true;
+        columns[j] = true;
       }
     }
   }
 
   for(var i = 0; i < rows.length; i++) {
-    var row = rows[i];
-    for(var j = 0; j < matrix[row].length; j++) {
-      matrix[row][j] = 0;
+    if(rows[i] === true) {
+      for(var j = 0; j < matrix[i].length; j++) {
+        matrix[i][j] = 0;
+      }
     }
   }
 
   for(var i = 0; i < columns.length; i++) {
-    var column = columns[i];
-    for(var j = 0; j < matrix.length; j++) {
-      matrix[j][column] = 0;
+    if(columns[i] === true) {
+      for(var j = 0; j < matrix.length; j++) {
+        matrix[j][i] = 0;
+      }
     }
   }
 
