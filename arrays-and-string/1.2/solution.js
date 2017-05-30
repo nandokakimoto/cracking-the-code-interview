ce0aacb71bd9a1f8f78a58023e94ccd620b270ca
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-05-29 22:16:12 -0300

    Fixes eslint errors to 1.2 problem.

--- arrays-and-string/1.2/solution.js
+++ arrays-and-string/1.2/solution.js
@@ -1,5 +1,7 @@
 module.exports = function(input1, input2) {
-  var bitVector = {};
+  var charMap = {};
+  var i;
+  var char;
 
   if(input1.length !== input2.length) {
     return false;
@@ -9,21 +11,21 @@ module.exports = function(input1, input2) {
     return false;
   }
 
-  for(var i = 0; i < input1.length; i++) {
-    var char = input1[i];
-    bitVector[char] = bitVector[char] ? bitVector[char] + 1 : 1;
+  for(i = 0; i < input1.length; i++) {
+    char = input1[i];
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
   }
 
-  for(var i = 0; i < input2.length; i++) {
-    var char = input2[i];
+  for(i = 0; i < input2.length; i++) {
+    char = input2[i];
 
-    if(!bitVector[char]) {
+    if(!charMap[char]) {
       return false;
     }
 
-    bitVector[char] -= 1;
+    charMap[char] -= 1;
 
-    if (bitVector[char] < 0) {
+    if (charMap[char] < 0) {
       return false;
     }
   }
