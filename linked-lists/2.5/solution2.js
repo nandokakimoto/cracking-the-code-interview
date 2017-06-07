342ff4564004017ef0bc02d0232f1c6e923397b1
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-06-06 23:03:52 -0300

    Adds recursive solution to 2.5 problem.

---
+++ linked-lists/2.5/solution2.js
@@ -0,0 +1,37 @@
+var Node = require('./lib/node.js');
+
+function sumList(head1, head2, overhead) {
+  var data1 = 0;
+  var data2 = 0;
+  var sum = 0;
+  var head = null;
+
+  if(head1 === null && head2 === null) {
+    return null;
+  }
+
+  if(head1 !== null) {
+    data1 = head1.data;
+    head1 = head1.next;
+  }
+
+  if(head2 !== null) {
+    data2 = head2.data;
+    head2 = head2.next;
+  }
+
+  sum = data1 + data2 + overhead;
+  head = new Node(sum % 10);
+
+  if(sum >= 10) {
+    head.next = sumList(head1, head2, 1);
+  } else {
+    head.next = sumList(head1, head2, 0);
+  }
+
+  return head;
+}
+
+module.exports = function(head1, head2) {
+  return sumList(head1, head2, 0);
+};
