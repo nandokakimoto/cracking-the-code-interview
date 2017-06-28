f70e8cda1f2975a9f429fd3ef972dc4930ee9494
Fernando Kakimoto <nandokakimoto@gmail.com>
2017-06-27 23:35:50 -0300

    Adds test cases regarding stack 2.

--- stacks-and-queues/3.1/test/solution_test.js
+++ stacks-and-queues/3.1/test/solution_test.js
@@ -15,6 +15,9 @@ describe('Three in One', () => {
       stacks.push(1, 15);
 
       assert(!stacks.isEmpty(1));
+      assert(stacks.isEmpty(2));
+      assert(stacks.isEmpty(3));
+
       assert.equal(15, stacks.pop(1));
       assert.equal(10, stacks.pop(1));
       assert.equal(5, stacks.pop(1));
@@ -40,12 +43,37 @@ describe('Three in One', () => {
 
   describe('second stack', () => {
     it('should allow push and pop operations', () => {
+      stacks.push(2, 5);
+      stacks.push(2, 10);
+      stacks.push(2, 20);
+      stacks.push(2, 15);
+
+      assert(stacks.isEmpty(1));
+      assert(!stacks.isEmpty(2));
+      assert(stacks.isEmpty(3));
+
+      assert.equal(15, stacks.pop(2));
+      assert.equal(20, stacks.pop(2));
+      assert.equal(10, stacks.pop(2));
+      assert.equal(5, stacks.pop(2));
     });
 
     it('should throw error if pushing above the stack limit', () => {
+      stacks.push(2, 5);
+      stacks.push(2, 10);
+      stacks.push(2, 20);
+      stacks.push(2, 15);
+
+      assert.throws(() => { stacks.push(2, 25); }, /Stack Overflow/);
     });
 
     it('should throw error if poping from empty stack', () => {
+      stacks.push(2, 5);
+      stacks.push(2, 10);
+
+      stacks.pop(2);
+      stacks.pop(2);
+      assert.throws(() => { stacks.pop(2); }, /Empty Stack/);
     });
   });
 
